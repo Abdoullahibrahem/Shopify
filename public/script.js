7b18bc87b5948b7f6eea3495fae15d865c9ca5af
Abdoullahibrahem <107931935+Abdoullahibrahem@users.noreply.github.com>
2022-07-28 13:51:06 +0200

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,54 @@
+
+$(document).ready(function(){
+    $(".startBtn1").click(function(e){
+      $(".List-drplist1").toggle();
+      $(".List-drplist2").hide();
+      $(".List-drplist3").hide();
+      $(".List-drplist4").hide();
+      $(".List-drplist5").hide();
+      
+    });
+});
+
+$(document).ready(function(){
+    $(".startBtn2").click(function(){
+        $(".List-drplist1").hide();
+        $(".List-drplist2").toggle();
+        $(".List-drplist3").hide();
+        $(".List-drplist4").hide();
+        $(".List-drplist5").hide();
+        
+    });
+});
+
+$(document).ready(function(){
+    $(".startBtn3").click(function(){
+        $(".List-drplist1").hide();
+        $(".List-drplist2").hide();
+        $(".List-drplist3").toggle();
+        $(".List-drplist4").hide();
+        $(".List-drplist5").hide();
+    
+    });
+});
+
+$(document).ready(function(){
+    $(".startBtn4").click(function(){
+        $(".List-drplist1").hide();
+        $(".List-drplist2").hide();
+        $(".List-drplist3").hide();
+        $(".List-drplist4").toggle();
+        $(".List-drplist5").hide();
+    });
+});
+$(document).ready(function(){
+    $(".startBtn5").click(function(){
+        $(".List-drplist1").hide();
+        $(".List-drplist2").hide();
+        $(".List-drplist3").hide();
+        $(".List-drplist4").hide();
+        $(".List-drplist5").toggle();
+  
+    });
+});
+
